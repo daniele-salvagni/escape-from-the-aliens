aa15b8968a27434b792df87c037145c25ff8c3fa
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-04-30 00:30:38 +0200

    Add hashCode and equals to CubicCoordinate

--- src/main/java/it/polimi/ingsw/cg_2/model/CubicCoordinate.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/CubicCoordinate.java
@@ -100,4 +100,29 @@ public final class CubicCoordinate {
         return z;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + z;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CubicCoordinate other = (CubicCoordinate) obj;
+        if (x != other.x)
+            return false;
+        if (z != other.z)
+            return false;
+        return true;
+    }
+
 }
