1130dcbb7de6c57a507c9ca65cf0bb1af5d0cf2c
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-04-30 09:16:26 +0200

    Add margin methods to ZoneHelper

--- src/main/java/it/polimi/ingsw/cg_2/utils/ZoneHelper.java
+++ src/main/java/it/polimi/ingsw/cg_2/utils/ZoneHelper.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /**
- * TODO: Complete javadoc
- * This class will be used to load/save maps.
+ * TODO: Complete javadoc This class will be used to load/save maps.
  */
 public class ZoneHelper {
 
@@ -19,6 +18,8 @@ public class ZoneHelper {
     private ZoneHelper() {
         throw new AssertionError();
     }
+
+    // // IMAGE I/O // //
 
     /**
      * Loads an image from file into a bidimensional integer array of pixels in
@@ -76,6 +77,101 @@ public class ZoneHelper {
 
         File outputFile = new File(filePath);
         ImageIO.write(bufferImage, "png", outputFile);
+    }
+
+    // // IMAGE PROCESSING // //
+
+    /**
+     * This method shifts down even columns of the hexagonal grid representation
+     * to produce a human readable and modifiable image with a better
+     * understanding of possible movements. The returned pixel matrix is 1px
+     * higher than the one in input, margins are filled in black color.
+     *
+     * @param pixelMatrix the input matrix
+     * @return a new matrix with shifted columns and 1px higher
+     */
+    public static int[][] addMargins(int[][] pixelMatrix) {
+
+        int imageWidth = pixelMatrix.length;
+        int imageHeight = pixelMatrix[0].length;
+
+        int[][] newPixelMatrix = new int[imageWidth][imageHeight + 1];
+
+        for (int x = 0; x < imageWidth; x++) {
+            for (int y = 0; y < imageHeight; y++) {
+                if ((x % 4 == 2) || (x % 4 == 3)) {
+
+                    /*
+                     * If columns 2, 3, 6, 7, 19, 11, 14, 15 ... then add a
+                     * black pixel on the top and shift the rest of the column.
+                     */
+
+                    if (y == 0) {
+                        newPixelMatrix[x][y] = 0x00000000;
+                    }
+
+                    newPixelMatrix[x][y + 1] = pixelMatrix[x][y];
+
+                } else {
+
+                    /* Else, just add a black pixel to the end. */
+
+                    newPixelMatrix[x][y] = pixelMatrix[x][y];
+
+                    if (y == imageHeight - 1) {
+                        newPixelMatrix[x][y - 1] = 0x00000000;
+                    }
+                }
+
+            }
+        }
+
+        return newPixelMatrix;
+    }
+
+    /**
+     * This method shifts up even columns of the hexagonal grid representation
+     * to remove the margins produced for better human understanding of possible
+     * movements. The returned pixel matrix is 1px less high than the one in
+     * input.
+     *
+     * @param pixelMatrix the input matrix
+     * @return a new matrix with shifted back columns and 1px less high
+     */
+    public static int[][] removeMargins(int[][] pixelMatrix) {
+
+        int imageWidth = pixelMatrix.length;
+        int imageHeight = pixelMatrix[0].length;
+
+        int[][] newPixelMatrix = new int[imageWidth][imageHeight - 1];
+
+        for (int x = 0; x < imageWidth; x++) {
+            for (int y = 0; y < imageHeight; y++) {
+                if ((x % 4 == 2) || (x % 4 == 3)) {
+
+                    /*
+                     * If columns 2, 3, 6, 7, 19, 11, 14, 15 ... then copy the
+                     * whole column except the first pixel and shift it up by 1.
+                     */
+
+                    if (y != 0) {
+                        newPixelMatrix[x][y - 1] = pixelMatrix[x][y];
+                    }
+
+                } else {
+
+                    /* Else, just copy the whole column except the last pixel. */
+
+                    if (y != imageHeight - 1) {
+                        newPixelMatrix[x][y] = pixelMatrix[x][y];
+                    }
+
+                }
+            }
+
+        }
+
+        return newPixelMatrix;
     }
 
 }
