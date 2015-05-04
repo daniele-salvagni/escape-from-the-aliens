3ab0f191460c6b11209ff497c7f3005b62d4bd13
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-04 12:40:05 +0200

    Add interface methods

--- src/main/java/it/polimi/ingsw/cg_2/model/deck/DecksFactory.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/deck/DecksFactory.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.cg_2.model.deck;
 
 public interface DecksFactory {
-    
+
+    DeckManager<ItemCard> createItemDeck();
+
+    DeckManager<SectorCard> createSectorDeck();
+
+    DeckManager<HatchCard> createHatchDeck();
 }
--- src/main/java/it/polimi/ingsw/cg_2/model/deck/StandardDecksFactory.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/deck/StandardDecksFactory.java
@@ -2,4 +2,22 @@ package it.polimi.ingsw.cg_2.model.deck;
 
 public class StandardDecksFactory implements DecksFactory {
 
+    @Override
+    public DeckManager<ItemCard> createItemDeck() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DeckManager<SectorCard> createSectorDeck() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DeckManager<HatchCard> createHatchDeck() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
