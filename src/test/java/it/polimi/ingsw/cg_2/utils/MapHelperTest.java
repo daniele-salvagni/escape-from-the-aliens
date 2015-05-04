a05cb51487a10d2473870fe767ff35793dcba04c
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-04 19:38:20 +0200

    Add new test cases

--- src/test/java/it/polimi/ingsw/cg_2/utils/MapHelperTest.java
+++ src/test/java/it/polimi/ingsw/cg_2/utils/MapHelperTest.java
@@ -7,14 +7,19 @@ import java.util.Arrays;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 public class MapHelperTest {
 
+    // // WELL FORMED ARGUMENTS // //
+
+    /** Folder used to save temporary test files. */
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     /**
      * Test the loading of a well-formed map.
-     *
-     * @throws Exception the exception
      */
     @Test
     public void testLoadNormalMap() throws Exception {
@@ -31,14 +36,8 @@ public class MapHelperTest {
         assertTrue(Arrays.deepEquals(loadedMatrix, expectedMatrix));
     }
 
-    /** Folder used to save temporary test files. */
-    @Rule
-    public TemporaryFolder tempFolder = new TemporaryFolder();
-
     /**
      * Test the saving on disk of a map.
-     *
-     * @throws Exception the exception
      */
     @Test
     public void testSaveNormalMap() throws Exception {
@@ -60,7 +59,57 @@ public class MapHelperTest {
         int[][] savedMatrix = MapHelper.loadMap(filePath);
 
         assertTrue(Arrays.deepEquals(inputMatrix, savedMatrix));
+    }
 
+    // // NULL ARGUMENT // //
+
+    /** The ExpectedException Rule. */
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /**
+     * Test the (@link MapHelper) loadMap method with a null argument.
+     */
+    @Test
+    public void testLoadNullMap() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        MapHelper.loadMap(null);
+    }
+
+    /**
+     * Test the (@link MapHelper) saveMap method with null arguments.
+     */
+    @Test
+    public void testWriteNullMap() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        MapHelper.saveMap(null, null);
+    }
+    
+    // // NOT WELL FORMED ARGUMENTS // //
+    
+    /**
+     * Test the saving on disk of a map.
+     */
+    @Test
+    public void testSaveNotWellFormedMap() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+
+        /* A column has 3 pixels instead of 2. */
+        int[][] inputMatrix = new int[][] { { 0xFF66CC66, 0xFF009966 },
+                { 0xFF993333, 0xFF0099CC, 0xFF993333 }, { 0xFF993399, 0xFF003333 } };
+
+        File outputFile = tempFolder.newFile("map-test-save-not-well-formed.png");
+        String filePath = outputFile.getPath();
+
+        MapHelper.saveMap(inputMatrix, filePath);
+
+        /*
+         * Use the already tested method loadMap on the created file and check
+         * if it equals the input matrix.
+         */
+        int[][] savedMatrix = MapHelper.loadMap(filePath);
+
+        assertTrue(Arrays.deepEquals(inputMatrix, savedMatrix));
     }
 
 }
