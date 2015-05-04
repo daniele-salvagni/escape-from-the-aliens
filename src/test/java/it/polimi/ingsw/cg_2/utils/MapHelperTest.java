2350140726b9636e9890c981a4a6d94ab62d6540
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-04 17:23:18 +0200

    Add junit test class

---
+++ src/test/java/it/polimi/ingsw/cg_2/utils/MapHelperTest.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.cg_2.utils;
+
+import org.junit.Test;
+
+public class MapHelperTest {
+
+    @Test
+    public void testLoadMap() throws Exception {
+        throw new RuntimeException("not yet implemented");
+    }
+
+    @Test
+    public void testSaveMap() throws Exception {
+        throw new RuntimeException("not yet implemented");
+    }
+
+}
