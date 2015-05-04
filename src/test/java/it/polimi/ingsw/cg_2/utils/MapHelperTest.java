05cb04ac2b2c3d4e38b86a2bbe02baeac52145cc
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-04 18:59:23 +0200

    Add test cases for loading/saving well-formed maps

--- src/test/java/it/polimi/ingsw/cg_2/utils/MapHelperTest.java
+++ src/test/java/it/polimi/ingsw/cg_2/utils/MapHelperTest.java
@@ -1,17 +1,66 @@
 package it.polimi.ingsw.cg_2.utils;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class MapHelperTest {
 
+    /**
+     * Test the loading of a well-formed map.
+     *
+     * @throws Exception the exception
+     */
     @Test
-    public void testLoadMap() throws Exception {
-        throw new RuntimeException("not yet implemented");
+    public void testLoadNormalMap() throws Exception {
+
+        /* Load the testing image. */
+        String filePath = getClass().getResource(
+                "/map/map-test-load-normal.png").getFile();
+        int[][] loadedMatrix = MapHelper.loadMap(filePath);
+
+        /* The expected matrix of pixels. */
+        int[][] expectedMatrix = new int[][] { { 0xFF66CC66, 0xFF009966 },
+                { 0xFF993333, 0xFF0099CC }, { 0xFF993399, 0xFF003333 } };
+
+        assertTrue(Arrays.deepEquals(loadedMatrix, expectedMatrix));
     }
 
+    /** Folder used to save temporary test files. */
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    /**
+     * Test the saving on disk of a map.
+     *
+     * @throws Exception the exception
+     */
     @Test
-    public void testSaveMap() throws Exception {
-        throw new RuntimeException("not yet implemented");
+    public void testSaveNormalMap() throws Exception {
+
+        /* The matrix we want to save. */
+        int[][] inputMatrix = new int[][] { { 0xFF66CC66, 0xFF009966 },
+                { 0xFF993333, 0xFF0099CC }, { 0xFF993399, 0xFF003333 } };
+
+        /* Saves the testing image. */
+        File outputFile = tempFolder.newFile("map-test-save-normal.png");
+        String filePath = outputFile.getPath();
+
+        MapHelper.saveMap(inputMatrix, filePath);
+
+        /*
+         * Use the already tested method loadMap on the created file and check
+         * if it equals the input matrix.
+         */
+        int[][] savedMatrix = MapHelper.loadMap(filePath);
+
+        assertTrue(Arrays.deepEquals(inputMatrix, savedMatrix));
+
     }
 
 }
