2ad3752803f3993b87d89160bd41d699b564c06e
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-05 22:55:17 +0200

    Add some basic methods (incomplete)

--- src/main/java/it/polimi/ingsw/cg_2/model/map/Zone.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/map/Zone.java
@@ -1,5 +1,68 @@
 package it.polimi.ingsw.cg_2.model.map;
 
+import it.polimi.ingsw.cg_2.model.deck.SectorCard.SectorCardType;
+import it.polimi.ingsw.cg_2.model.map.Sector.SectorType;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
 public class Zone {
-    // TODO: Create class Zone
+    Map<CubicCoordinate, Sector> sectors;
+
+    // TODO: Incomplete
+
+    public Map<CubicCoordinate, Sector> getSectors() {
+        return sectors;
+    }
+
+    public Sector getHumanSector() {
+        Sector humanSector = findSectorFromType(SectorType.HUMAN);
+        if (humanSector == null) {
+            throw new NullPointerException(
+                    "A Zone must contain an human sector.");
+        } else {
+            return humanSector;
+        }
+    }
+
+    public Sector getAlienSector() {
+        Sector alienSector = findSectorFromType(SectorType.ALIEN);
+        if (alienSector == null) {
+            throw new NullPointerException(
+                    "A Zone must contain an alien sector.");
+        } else {
+            return alienSector;
+        }
+    }
+
+    public ArrayList<Sector> getHatchSectors() {
+        ArrayList<Sector> hatchSectors = findSectorsFromType(SectorType.HATCH);
+        if (hatchSectors.isEmpty()) {
+            throw new NullPointerException(
+                    "A Zone must contain at least one escape hatch.");
+        } else {
+            return hatchSectors;
+        }
+    }
+
+    private Sector findSectorFromType(SectorType sectorType) {
+        for (Sector s : sectors.values()) {
+            if (s.getType() == sectorType) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    private ArrayList<Sector> findSectorsFromType(SectorType sectorType) {
+        ArrayList<Sector> foundSectors = new ArrayList<Sector>();
+        for (Sector s : sectors.values()) {
+            if (s.getType() == sectorType) {
+                foundSectors.add(s);
+            }
+        }
+        return foundSectors;
+    }
+
 }
