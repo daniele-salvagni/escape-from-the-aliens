f44693d1023d27645c195d9d5592317c4048e92e
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-08 17:44:07 +0200

    Add hashCode, equals & toString

--- src/main/java/it/polimi/ingsw/cg_2/model/map/Sector.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/map/Sector.java
@@ -39,4 +39,38 @@ public class Sector {
         return position;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((position == null) ? 0 : position.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Sector other = (Sector) obj;
+        if (position == null) {
+            if (other.position != null)
+                return false;
+        } else if (!position.equals(other.position))
+            return false;
+        if (type != other.type)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Sector [type=" + type + ", position=" + position + "]";
+    }
+
 }
