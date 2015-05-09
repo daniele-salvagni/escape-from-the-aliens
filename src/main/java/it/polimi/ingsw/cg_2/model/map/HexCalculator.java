0e9a5bedd263c8b1347923cbddcfecaddd6c5301
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-10 00:07:57 +0200

    Add HexCalculator class

---
+++ src/main/java/it/polimi/ingsw/cg_2/model/map/HexCalculator.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.cg_2.model.map;
+
+/**
+ * This class models the constraints of an hexagonal map, the algorithms are
+ * implemented using {@link CubicCoordinate}. Some algorithm in this class are a
+ * Java implementation of the work done by <b>Amit Patel</b>, for more
+ * informations see <a href="http://www.redblobgames.com/grids/hexagons/">his
+ * website</a>.
+ */
+public class HexCalculator {
+
+}
