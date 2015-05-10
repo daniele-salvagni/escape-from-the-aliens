18b5846e2bcd1f8c7f62528ed264548d6e396156
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-10 13:51:10 +0200

    Add new test cases

--- src/test/java/it/polimi/ingsw/cg_2/model/deck/DeckManagerTest.java
+++ src/test/java/it/polimi/ingsw/cg_2/model/deck/DeckManagerTest.java
@@ -5,8 +5,11 @@ import it.polimi.ingsw.cg_2.model.deck.HatchCard.HatchCardType;
 import it.polimi.ingsw.cg_2.model.deck.ItemCard.ItemCardType;
 import it.polimi.ingsw.cg_2.model.deck.SectorCard.SectorCardType;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,45 +17,77 @@ import org.junit.Test;
 public class DeckManagerTest {
 
     private DeckManager<Card> deck;
-    private Collection<Card> cards;
+    private List<Card> cards;
 
     @Before
     public void init() {
+
         /*
          * Creates a Set of cards, mixing item cards, sector cards and hatch
          * cards to populate the deck.
          */
-        cards = new HashSet<Card>();
+        cards = new ArrayList<Card>();
         cards.add(new ItemCard(ItemCardType.ADRENALINE));
         cards.add(new SectorCard(SectorCardType.NOISE));
         cards.add(new HatchCard(HatchCardType.RED));
 
         /* Instantiate a new deck populated with some test cards. */
         deck = new DeckManager<Card>(cards);
+
     }
 
     @Test
     public void shouldBeCreated() throws Exception {
+        
         assertNotNull(deck);
+        
     }
 
     @Test
-    public void testDrawCard() throws Exception {
+    public void shouldDrawCards() throws Exception {
+        
+        // We expect cards to be drawn in reverse order (works like a stack)
+        Collections.reverse(cards);
+        for (Card card : cards) {
+            assertEquals(card, deck.drawCard());
+        }
+        
+    }
+
+    @Test
+    public void shouldDrawNullIfEmpty() throws Exception {
+        
+        // We empty the deck
+        for (int i = 0; i < cards.size(); i++) {
+            deck.drawCard();
+        }
+        assertNull(deck.drawCard());
+        
+    }
+
+    @Test
+    public void shouldUseDiscardPileIfDeckIsEmpty() throws Exception {
+        
+        // We empty the deck except for one card
+        for (int i = 0; i < (cards.size() - 1); i++) {
+            deck.drawCard();
+        }
+        // We empty the deck but we save a reference to the last card
+        Card expectedCard = deck.drawCard();
+        // We put it back in the discard pile of the deck
+        deck.discardCard(expectedCard);
+        // We check if we get the same card
+        assertEquals(expectedCard, deck.drawCard());
+        
+    }
+
+    @Test
+    public void shouldReturnTrueIfEmpry() throws Exception {
         throw new RuntimeException("not yet implemented");
     }
 
     @Test
-    public void testDiscardCard() throws Exception {
-        throw new RuntimeException("not yet implemented");
-    }
-
-    @Test
-    public void testIsDeckEmpty() throws Exception {
-        throw new RuntimeException("not yet implemented");
-    }
-
-    @Test
-    public void testIsEmpty() throws Exception {
+    public void shouldReturnTrueIfDiscardIsEmpty() throws Exception {
         throw new RuntimeException("not yet implemented");
     }
 
