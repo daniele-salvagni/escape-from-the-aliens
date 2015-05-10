021c96c00a711e29d40f056973fb203036241083
Simone Triventi <simone.triventi@gmail.com>
2015-05-10 16:21:36 +0200

    Implement class methods

--- src/main/java/it/polimi/ingsw/cg_2/model/player/CharacterRank.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/player/CharacterRank.java
@@ -1,5 +1,27 @@
 package it.polimi.ingsw.cg_2.model.player;
 
-public class CharacterRank {
+import static it.polimi.ingsw.cg_2.model.player.CharacterType.*;
+
+public enum CharacterRank {
+
+	CAPTAIN(HUMAN),
+	PILOT(HUMAN),
+	PSYCHOLOGIST(HUMAN),
+	SOLDIER(HUMAN),
+	FIRST(ALIEN),
+	SECOND(ALIEN),
+	THIRD(ALIEN),
+	FOURTH(ALIEN);
+
+	private CharacterType defaultType;
+
+	CharacterRank(CharacterType defaultType) {
+		this.defaultType = defaultType;
+	}
+	
+	public CharacterType getDefaultType(){
+		
+		return defaultType;
+	}
 
 }
