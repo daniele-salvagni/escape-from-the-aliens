880941d331957458f4a3375895d3457f28ad8110
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-11 17:45:12 +0200

    Remove hashCode and equals override
    We don't need equals() on a Sector (there will be only one sector in a
    certain coordinate).

--- src/main/java/it/polimi/ingsw/cg_2/model/map/Sector.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/map/Sector.java
@@ -10,7 +10,11 @@ public class Sector {
      * The kinds of Sector type.
      */
     public static enum SectorType {
-        SAFE, DANGEROUS, HUMAN, ALIEN, HATCH;
+        SAFE,
+        DANGEROUS,
+        HUMAN,
+        ALIEN,
+        HATCH;
     }
 
     private SectorType type;
@@ -43,35 +47,6 @@ public class Sector {
      */
     public CubicCoordinate getCooridnate() {
         return position;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((position == null) ? 0 : position.hashCode());
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Sector other = (Sector) obj;
-        if (position == null) {
-            if (other.position != null)
-                return false;
-        } else if (!position.equals(other.position))
-            return false;
-        if (type != other.type)
-            return false;
-        return true;
     }
 
     @Override
