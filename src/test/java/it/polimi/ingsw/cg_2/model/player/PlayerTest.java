3a9c116ac6a39c0af580d66771c1d97e323c6722
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-12 21:22:09 +0200

    Implement test cases

--- src/test/java/it/polimi/ingsw/cg_2/model/player/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/cg_2/model/player/PlayerTest.java
@@ -1,14 +1,61 @@
 package it.polimi.ingsw.cg_2.model.player;
 
+import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
+import it.polimi.ingsw.cg_2.model.map.Sector;
+import it.polimi.ingsw.cg_2.model.map.Sector.SectorType;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
+    private SectorType sectorType;
+    private CubicCoordinate coordinate;
+    private Sector sector;
+    private Character character;
+
+    private Player player;
+
+    @Before
+    public void init() {
+
+        sectorType = SectorType.SAFE;
+        coordinate = CubicCoordinate.create(0, 0, 0);
+        sector = new Sector(coordinate, sectorType);
+        character = new Character(sector, CharacterRank.CAPTAIN);
+
+        player = new Player(character);
+
+    }
+
     @Test
-    public void testPlayer() throws Exception {
+    public void shouldBeCreated() {
+
+        assertNotNull(player);
+
+    }
+
+    @Test
+    public void shouldGetCharacter() {
+
+        assertEquals(player.getCharacter(), character);
+
+    }
+
+    @Test
+    public void shouldMoveCharacter() {
+
         // TODO
+
+    }
+
+    @Test
+    public void shouldUpdateMovementHistory() {
+
+        // TODO
+
     }
 
 }
