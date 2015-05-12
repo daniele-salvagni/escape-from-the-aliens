e468a4c67997fc29c27934ec0dbbf1ed9c0808de
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-12 20:48:51 +0200

    Create Player test class

---
+++ src/test/java/it/polimi/ingsw/cg_2/model/player/PlayerTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.cg_2.model.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testPlayer() throws Exception {
+        throw new RuntimeException("not yet implemented");
+    }
+
+}
