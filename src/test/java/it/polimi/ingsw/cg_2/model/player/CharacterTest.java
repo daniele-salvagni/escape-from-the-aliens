4f741d5b029864a1bccd3967b850bcd6218dad91
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-12 20:20:36 +0200

    Add Character test class

---
+++ src/test/java/it/polimi/ingsw/cg_2/model/player/CharacterTest.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.cg_2.model.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CharacterTest {
+
+    @Test
+    public void testCharacterSectorCharacterRankCharacterRace()
+            throws Exception {
+        throw new RuntimeException("not yet implemented");
+    }
+
+}
