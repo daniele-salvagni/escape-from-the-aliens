9b5a57abfa7a7f5cf3bbae7c3550b18ea45a38f6
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-12 21:33:28 +0200

    Implement class methods

--- src/main/java/it/polimi/ingsw/cg_2/model/noise/Noise.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/noise/Noise.java
@@ -1,5 +1,38 @@
 package it.polimi.ingsw.cg_2.model.noise;
 
+import it.polimi.ingsw.cg_2.model.map.Sector;
+import it.polimi.ingsw.cg_2.model.player.Player;
+
 public class Noise {
+
+    private final Player player;
+    private final Sector sector;
+    private final int turnNumber;
+
+    public Noise(Player player, Sector sector, int turnNumber) {
+
+        this.player = player;
+        this.sector = sector;
+        this.turnNumber = turnNumber;
+
+    }
+
+    public Player getPlayer() {
+
+        return player;
+
+    }
+
+    public Sector getSector() {
+
+        return sector;
+
+    }
+
+    public int getTurnNumber() {
+
+        return turnNumber;
+
+    }
 
 }
