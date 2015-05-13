3f6d89b4aca28e64b7e7e7e09234c3cfdcedbf73
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-13 10:21:11 +0200

    Add Zone test class

---
+++ src/test/java/it/polimi/ingsw/cg_2/model/map/ZoneTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.cg_2.model.map;
+
+import org.junit.Test;
+
+public class ZoneTest {
+
+    @Test
+    public void testZone() throws Exception {
+
+        // TODO
+    }
+
+}
