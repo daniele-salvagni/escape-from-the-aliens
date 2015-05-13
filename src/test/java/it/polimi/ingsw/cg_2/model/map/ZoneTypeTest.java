2646fd11318e04f8d55c62229f0c89352c6a5a50
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-13 10:14:02 +0200

    Add literals test cases

--- src/test/java/it/polimi/ingsw/cg_2/model/map/ZoneTypeTest.java
+++ src/test/java/it/polimi/ingsw/cg_2/model/map/ZoneTypeTest.java
@@ -2,9 +2,25 @@ package it.polimi.ingsw.cg_2.model.map;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class ZoneTypeTest {
+
+    @Test
+    public void shouldContainLiterals() {
+
+        assertNotNull(ZoneType.values().length > 0);
+
+    }
+
+    @Test
+    public void shouldContainPNGFileNames() {
+
+        for (ZoneType type : ZoneType.values()) {
+            assertTrue(type.getFileName().contains(".png")
+                    || type.getFileName().contains(".PNG"));
+        }
+
+    }
 
 }
