2766440b11e0e44ac8ce29fb883dc036581a986c
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-13 19:11:38 +0200

    Add initial seed generation

--- src/main/java/it/polimi/ingsw/cg_2/utils/procedural/CellularAutomata.java
+++ src/main/java/it/polimi/ingsw/cg_2/utils/procedural/CellularAutomata.java
@@ -1,24 +1,32 @@
 package it.polimi.ingsw.cg_2.utils.procedural;
 
 import static it.polimi.ingsw.cg_2.utils.procedural.CellStatus.*;
-
 import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class CellularAutomata {
 
+    private final int ALIVE_CHANCE = 45;
+
+    long seed;
+    int gridWidth;
+    int gridHeight;
+
+    Random rand; // With SEED
+
+    // To initialize as LinkedHashMap (we MUST preserve order)
+    Map<CubicCoordinate, CellStatus> grid;
+
     // Default 23x14
-    public static Map<CubicCoordinate, CellStatus> generateRectangularGrid(
-            int width, int height) {
+    protected void generateRectangularGrid() {
 
-        Map<CubicCoordinate, CellStatus> grid = new HashMap<>();
-
-        for (int col = 0; col < width; col++) {
-            for (int row = 0; row < height; row++) {
+        for (int col = 0; col < gridWidth; col++) {
+            for (int row = 0; row < gridHeight; row++) {
                 CubicCoordinate coord = CubicCoordinate
                         .createFromOddQ(col, row);
                 grid.put(coord, DEAD);
@@ -26,7 +34,15 @@ public class CellularAutomata {
 
         }
 
-        return grid;
+    }
+
+    protected void ceateInitialSeed() {
+
+        for (Map.Entry<CubicCoordinate, CellStatus> cell : grid.entrySet()) {
+            if (rand.nextInt(100) < ALIVE_CHANCE) {
+                cell.setValue(ALIVE);
+            }
+        }
 
     }
 
