39aa9071f40c4021093ad5f8b0c8265c75b8f3c3
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-13 18:45:30 +0200

    Change Set to Map

--- src/main/java/it/polimi/ingsw/cg_2/utils/procedural/CellularAutomata.java
+++ src/main/java/it/polimi/ingsw/cg_2/utils/procedural/CellularAutomata.java
@@ -1,23 +1,27 @@
 package it.polimi.ingsw.cg_2.utils.procedural;
 
+import static it.polimi.ingsw.cg_2.utils.procedural.CellStatus.*;
+
 import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class CellularAutomata {
 
     // Default 23x14
-    public static Set<CubicCoordinate> generateRectangularGrid(int width,
-            int height) {
+    public static Map<CubicCoordinate, CellStatus> generateRectangularGrid(
+            int width, int height) {
 
-        Set<CubicCoordinate> grid = new HashSet<>();
+        Map<CubicCoordinate, CellStatus> grid = new HashMap<>();
 
         for (int col = 0; col < width; col++) {
             for (int row = 0; row < height; row++) {
                 CubicCoordinate coord = CubicCoordinate
                         .createFromOddQ(col, row);
-                grid.add(coord);
+                grid.put(coord, DEAD);
             }
 
         }
