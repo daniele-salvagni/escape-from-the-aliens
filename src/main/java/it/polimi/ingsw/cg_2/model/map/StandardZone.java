b3190445ba284a005662c035a0bed994677ad1fe
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-13 13:01:00 +0200

    Revert "Implement Zone interface"
    This reverts commit 990301773a0b70a23445da10d3e937929aa4734b.

--- src/main/java/it/polimi/ingsw/cg_2/model/map/StandardZone.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/map/StandardZone.java
@@ -8,13 +8,19 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * This class represents a standard implementation of a {@link Zone}.
+ * This class represents a StandardZone (the map of this game), this zone should
+ * contain:
+ * <ul>
+ * <li>Exactly one HUMAN Sector.</li>
+ * <li>Exactly one ALIEN Sector.</li>
+ * <li>At least one HATCH sector.</li>
+ * </ul>
  */
-public class StandardZone implements Zone {
+public class StandardZone {
 
     /**
-     * A map containing all the sectors of the StandardZone. A Sector also
-     * contain its coordinate but we provide this map for faster access.
+     * A map containing all the sectors of the StandardZone. A Sector also contain its
+     * coordinate but we provide this for faster access.
      */
     private final Map<CubicCoordinate, Sector> sectorMap;
 
@@ -39,7 +45,12 @@ public class StandardZone implements Zone {
 
     }
 
-    @Override
+    /**
+     * Gets all the sectors of the StandardZone mapped by CubicCoordinate. We return a
+     * copy of the Map to minimize mutability.
+     *
+     * @return a new Map containing all the sectors of the zone
+     */
     public Map<CubicCoordinate, Sector> getSectorsMap() {
 
         /*
@@ -53,14 +64,25 @@ public class StandardZone implements Zone {
 
     }
 
-    @Override
+    /**
+     * Gets the sector correspondent to a given coordinate.
+     *
+     * @param coord the coordinate to search for
+     * @return if present, the sector, otherwise null
+     */
     public Sector getSector(CubicCoordinate coord) {
 
         return sectorMap.get(coord);
 
     }
 
-    @Override
+    /**
+     * Gets a set containing all the coordinates of the sectors (of any kind) in
+     * this StandardZone. We return a copy of the Set used in the Map implementation to
+     * minimize mutability.
+     *
+     * @return the coordinates of this zone
+     */
     public Set<CubicCoordinate> getCoordinates() {
 
         /*
@@ -74,7 +96,13 @@ public class StandardZone implements Zone {
 
     }
 
-    @Override
+    /**
+     * Gets the human sector. Throws an exception if the sector has not been
+     * found or if more than one has been found. (A StandardZone should contain exactly
+     * one HUMAN sector).
+     *
+     * @return the human sector
+     */
     public Sector getHumanSector() {
 
         Set<Sector> humanSectors = findSectorsFromType(SectorType.HUMAN);
@@ -88,7 +116,13 @@ public class StandardZone implements Zone {
 
     }
 
-    @Override
+    /**
+     * Gets the alien sector. Throws an exception if the sector has not been
+     * found or if more than one has been found. (A StandardZone should contain exactly
+     * one ALIEN sector).
+     *
+     * @return the alien sector
+     */
     public Sector getAlienSector() {
 
         Set<Sector> alienSectors = findSectorsFromType(SectorType.ALIEN);
@@ -102,7 +136,12 @@ public class StandardZone implements Zone {
 
     }
 
-    @Override
+    /**
+     * Gets all the hatch sectors contained in this zone. (A StandardZone should contain
+     * at least one HATCH sector).
+     *
+     * @return the hatch sectors
+     */
     public Set<Sector> getHatchSectors() {
 
         Set<Sector> hatchSectors = findSectorsFromType(SectorType.HATCH);
@@ -111,7 +150,6 @@ public class StandardZone implements Zone {
             throw new IllegalStateException(
                     "A StandardZone must contain at least one escape hatch.");
         } else {
-
             return hatchSectors;
         }
 
