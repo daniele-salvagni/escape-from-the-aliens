a37c001b039effbe15fbab378318a7ceaef9b627
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-13 18:18:11 +0200

    Create CellularAutomata class

---
+++ src/main/java/it/polimi/ingsw/cg_2/utils/CellularAutomata.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.cg_2.utils;
+
+import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CellularAutomata {
+
+}
