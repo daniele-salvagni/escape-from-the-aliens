ee58043b7134490af07c5c70b8cb86f6fb0a2c8f
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-15 14:09:47 +0200

    Refactor: add class GridProcessor

---
+++ src/main/java/it/polimi/ingsw/cg_2/utils/procedural/GridProcessor.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.cg_2.utils.procedural;
+
+import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class contains the methods for processing grids with various algorithms.
+ */
+public class GridProcessor {
+
+}
