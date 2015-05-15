06cec211f88119e3a50eb901eb086e362e295628
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-15 14:14:35 +0200

    Add refactored flood fill algorithm

--- src/main/java/it/polimi/ingsw/cg_2/utils/procedural/GridProcessor.java
+++ src/main/java/it/polimi/ingsw/cg_2/utils/procedural/GridProcessor.java
@@ -1,8 +1,11 @@
 package it.polimi.ingsw.cg_2.utils.procedural;
 
 import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
+import it.polimi.ingsw.cg_2.model.map.HexCalculator;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -33,6 +36,90 @@ public class GridProcessor {
         }
 
         return foundCells;
+
+    }
+
+    /**
+     * Performs a flood fill algorithm on a grid from a certain starting point.
+     * It returns all the cells clustered against the starting one.
+     *
+     * @param grid the grid where to perform the flood fill
+     * @param start the starting point of the algorithm
+     * @param status the status on which to perform the flood fill
+     * @param distanceLimit the distance limit of the algorithm, if set to 0 it
+     *            will continue until the cluster has been completely filled
+     * @return a set containing all the found cells of the cluster
+     */
+    private static Set<CubicCoordinate> floodFill(
+            Map<CubicCoordinate, CellStatus> grid, CubicCoordinate start,
+            CellStatus status, int distanceLimit) {
+
+        // If distanceLimit is equals to 0 we perform an unlimited flood fill
+        boolean limited = distanceLimit != 0 ? false : true;
+
+        // A temporary set of coordinates, it will contain all the cells
+        // of this cluster
+        Set<CubicCoordinate> cluster = new HashSet<>();
+
+        // We add the starting cell to the cluster
+        cluster.add(start);
+
+        // This list will contain lists of cells with a certain distance
+        // from the starting cell
+        List<List<CubicCoordinate>> fringes = new ArrayList<List<CubicCoordinate>>();
+
+        // We initialize the first element that will contain the cells
+        // with distance 0 (the starting point)
+        fringes.add(new ArrayList<CubicCoordinate>());
+
+        // We add the starting cell also to the list of cells with
+        // distance 0
+        fringes.get(0).add(start);
+
+        int distance = 1;
+        // We loop until we filled an entire cluster, if the previous
+        // iteration did not found any neighbor with the given status we
+        // exit the loop. We also exit at a certain distance if the flood fill
+        // is limited.
+        while (!(fringes.get(fringes.size() - 1).isEmpty())
+                || ((limited == true) && (distance <= distanceLimit))) {
+
+            fringes.add(new ArrayList<CubicCoordinate>());
+
+            // For each coordinate found in the previous iteration we
+            // check if it has neighbors with the given status
+            for (CubicCoordinate coord : fringes.get(distance - 1)) {
+
+                // We try the 6 possible directions
+                for (int d = 0; d < 6; d++) {
+
+                    // We get the neighbor coordinate in the given
+                    // direction
+                    CubicCoordinate neighbor = HexCalculator.neighbor(coord, d);
+
+                    // We check if the neighbor has the given status and
+                    // if we did not already visited it
+                    if (grid.get(neighbor) == status
+                            && !cluster.contains(neighbor)) {
+
+                        // Add it to the cluster
+                        cluster.add(neighbor);
+                        // Add it to the cells with a certain distance
+                        // from the starting point
+                        fringes.get(distance).add(neighbor);
+
+                    }
+
+                }
+
+            }
+
+            // Increase the distance for the next loop
+            distance++;
+
+        }
+
+        return cluster;
 
     }
 
