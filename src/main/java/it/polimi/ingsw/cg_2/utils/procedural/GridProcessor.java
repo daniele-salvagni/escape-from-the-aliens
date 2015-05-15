1ebea0f39c6f4987ff14467a92faeb3323b94e12
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-15 14:10:27 +0200

    Add method to find cells in a given status

--- src/main/java/it/polimi/ingsw/cg_2/utils/procedural/GridProcessor.java
+++ src/main/java/it/polimi/ingsw/cg_2/utils/procedural/GridProcessor.java
@@ -11,4 +11,29 @@ import java.util.Set;
  */
 public class GridProcessor {
 
+    /**
+     * Gets all the cells of the grid with a given status.
+     *
+     * @param status the status to search for
+     * @return the cells with the given status
+     */
+    public static Set<CubicCoordinate> findCellsWithStatus(
+            Map<CubicCoordinate, CellStatus> grid, CellStatus status) {
+
+        Set<CubicCoordinate> foundCells = new HashSet<CubicCoordinate>();
+
+        // Loop trough the entire grid
+        for (Map.Entry<CubicCoordinate, CellStatus> entry : grid.entrySet()) {
+
+            // Check the status of the cell and add it to the found ones
+            if (entry.getValue() == status) {
+                foundCells.add(entry.getKey());
+            }
+
+        }
+
+        return foundCells;
+
+    }
+
 }
