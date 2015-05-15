c7f0881a8acd569eea10a967816a10b86762ddc8
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-15 12:09:00 +0200

    Add SeedGenerator test class with test cases

---
+++ src/test/java/it/polimi/ingsw/cg_2/utils/procedural/SeedGeneratorTest.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw.cg_2.utils.procedural;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
+
+import org.junit.Test;
+
+/**
+ * WARNING: The tested class involves randomness, so we can't exactly check if
+ * the output is correct, to properly test those methods a statistical analysis
+ * should be done instead.
+ */
+public class SeedGeneratorTest {
+
+    @Test
+    public void shouldGenerateAlteredSeed() {
+
+        long noSeed = -1;
+        Map<CubicCoordinate, CellStatus> seed = SeedGenerator
+                .generateAlteredSeed(100, 0, CellStatus.ALIVE, CellStatus.DEAD,
+                        noSeed);
+
+        /*
+         * We did set a 100% probability in the outside and a 0% probability in
+         * the inside, so it should always contain alive and dead cells.
+         */
+
+        assertTrue(seed.containsValue(CellStatus.ALIVE));
+        assertTrue(seed.containsValue(CellStatus.DEAD));
+
+    }
+
+    @Test
+    public void shouldGenerateUnalteredSeed() {
+
+        long noSeed = -1;
+        Map<CubicCoordinate, CellStatus> seed = SeedGenerator
+                .generateAlteredSeed(100, -1, CellStatus.ALIVE,
+                        CellStatus.DEAD, noSeed);
+
+        assertTrue(seed.containsValue(CellStatus.ALIVE));
+        assertFalse(seed.containsValue(CellStatus.DEAD));
+
+    }
+
+    @Test
+    public void shouldAcceptSeed() {
+
+        assertNotNull(SeedGenerator.generateAlteredSeed(30, 85,
+                CellStatus.ALIVE, CellStatus.DEAD, 123));
+    }
+}
