b69a0e29c3dcea5e7b2813afff942bb893b4a440
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-15 11:26:20 +0200

    Refactor: Add generateRectangularGrid(..)

--- src/main/java/it/polimi/ingsw/cg_2/utils/procedural/SeedGenerator.java
+++ src/main/java/it/polimi/ingsw/cg_2/utils/procedural/SeedGenerator.java
@@ -1,6 +1,47 @@
 package it.polimi.ingsw.cg_2.utils.procedural;
 
+import static it.polimi.ingsw.cg_2.model.map.CubicCoordinate.createFromOddQ;
+import static it.polimi.ingsw.cg_2.utils.procedural.CellStatus.ALIVE;
+import static it.polimi.ingsw.cg_2.utils.procedural.CellStatus.DEAD;
+import it.polimi.ingsw.cg_2.model.map.CubicCoordinate;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * This class server as a helper for the CellularAutomata, it provides static
+ * methods to generate seeds to be passed to the automata.
+ **/
 public class SeedGenerator {
+
+    private static final int CELL_WIDTH = 23;
+    private static final int CELL_HEIGHT = 14;
+
+    private Random rand;
+
+    /**
+     * Generates a grid of cells with a rectangular shape.
+     *
+     * @param cellStatus the {@link CellStatus} of the cells
+     * @return a map containing all the cells and their status
+     */
+    private Map<CubicCoordinate, CellStatus> generateRectangularGrid(
+            CellStatus cellStatus) {
+
+        Map<CubicCoordinate, CellStatus> rectGrid = new LinkedHashMap<>();
+
+        for (int col = 0; col < CELL_WIDTH; col++) {
+            for (int row = 0; row < CELL_HEIGHT; row++) {
+                // It is easy with OddQ coordinates
+                CubicCoordinate coord = createFromOddQ(col, row);
+                rectGrid.put(coord, cellStatus);
+            }
+
+        }
+
+        return rectGrid;
+
+    }
 
 }
