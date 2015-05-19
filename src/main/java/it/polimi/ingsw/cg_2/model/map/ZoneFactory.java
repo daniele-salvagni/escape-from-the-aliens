2df567f9ec06aaa6384c8a9fa194f913310048c1
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-19 16:52:12 +0200

    Add ZoneFactory interface

---
+++ src/main/java/it/polimi/ingsw/cg_2/model/map/ZoneFactory.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.cg_2.model.map;
+
+import java.util.Map;
+
+/**
+ * An abstract factory for creating Zones, it can be implemented to create
+ * different kinds of Zones with certain requirements.
+ */
+public interface ZoneFactory {
+
+
+
+}
