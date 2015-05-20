98467e834fc766d54ba3aacd16c9350a475896f0
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-20 21:42:33 +0200

    Implement test cases

--- src/test/java/it/polimi/ingsw/cg_2/model/map/ZoneFactoryTest.java
+++ src/test/java/it/polimi/ingsw/cg_2/model/map/ZoneFactoryTest.java
@@ -10,16 +10,62 @@ import org.junit.Test;
 
 public class ZoneFactoryTest {
 
-    private static ZoneFactory zoneLoader;
+    private static ZoneFactory zoneFactory;
     private static Zone zone;
 
     @BeforeClass
     public static void initClass() {
 
-        zoneLoader = new ZoneLoader(ZoneName.DILEMMA);
-        zone = zoneLoader.createZone();
+        zoneFactory = new ZoneLoader(ZoneName.DILEMMA);
+        zone = zoneFactory.createZone();
 
     }
 
+    @Test
+    public void shouldCreateFactory() throws Exception {
+
+        assertNotNull(zoneFactory);
+
+    }
+
+    @Test
+    public void shouldCreateZone() {
+
+        assertNotNull(zone);
+    }
+
+    @Test
+    public void shouldCreateInstanceOfZoneLoader() {
+
+        ZoneFactory zoneLoader = new ZoneLoader(ZoneName.DILEMMA);
+        assertTrue(zoneLoader instanceof ZoneLoader);
+
+    }
+
+    @Test
+    public void shouldCreateSector() {
+
+        SectorType type = SectorType.ALIEN;
+        Sector sector = zoneFactory.createSector(
+                CubicCoordinate.create(0, 0, 0), type);
+
+        assertNotNull(sector);
+        assertEquals(type, sector.getType());
+        assertEquals(CubicCoordinate.create(0, 0, 0), sector.getCooridnate());
+
+    }
+
+    @Test
+    public void hatchSectorShouldBeCloseable() {
+
+        /* Every hatch should be created of the subclass CloseableSector. */
+
+        SectorType type = SectorType.HATCH;
+        Sector sector = zoneFactory.createSector(
+                CubicCoordinate.create(0, 0, 0), type);
+
+        assertTrue(sector instanceof CloseableSector);
+
+    }
 
 }
