d96f5594472cb1a889955ebdc864cefa8fac75d4
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-22 21:50:29 +0200

    Create Game class (Model)

---
+++ src/main/java/it/polimi/ingsw/cg_2/model/Game.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.cg_2.model;
+
+/**
+ * This class represents the complete status of a Game, from here it is
+ * possible
+ * to access the whole model of the application.
+ */
+public class Game {
+
+}
