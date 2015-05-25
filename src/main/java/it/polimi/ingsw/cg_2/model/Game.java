371fbc7e280a7d404b2efe05f9618a57e721ef52
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-05-25 19:53:43 +0200

    Implement the Game static factory

--- src/main/java/it/polimi/ingsw/cg_2/model/Game.java
+++ src/main/java/it/polimi/ingsw/cg_2/model/Game.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.cg_2.model.map.Zone;
 import it.polimi.ingsw.cg_2.model.map.ZoneFactory;
 import it.polimi.ingsw.cg_2.model.noise.Noise;
 import it.polimi.ingsw.cg_2.model.player.Player;
+import it.polimi.ingsw.cg_2.model.player.PlayersFactory;
+import it.polimi.ingsw.cg_2.model.player.StandardPlayersFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,25 +29,63 @@ public class Game {
     private int turnNumber;
     private Player currentPlayer;
 
-    public Game(ZoneFactory zone, DecksFactory decks) {
+    /**
+     * The private constructor of the game, it does instantiate instantiates all
+     * the class attributes.
+     *
+     * @param zFactory an object implementation of a {@link ZoneFactory} used
+     *                 to
+     *                 create the map of the Game
+     * @param dFactory an object implementation of a {@link DecksFactory} used
+     *                 to populate all the decks of the Game
+     * @param pFactory an object implementation of a {@link PlayersFactory}
+     *                 used
+     *                 to populate the list of players of the Game
+     * @param pNumber  the number of players in this Game
+     */
+    private Game(ZoneFactory zFactory, DecksFactory dFactory, PlayersFactory
+            pFactory, int pNumber) {
 
-        this.zone = zone.createZone();
+        /* The zone factory, the parameters to create the zone (type, etc)
+        are already set, so we just instantiate a new zone. (details depends
+        on the implementation) */
+        this.zone = zFactory.createZone();
 
-        this.itemDeck = decks.createItemDeck();
-        this.sectorDeck = decks.createSectorDeck();
-        this.hatchDeck = decks.createHatchDeck();
+        /* The decks factory is able to correctly populate all the decks
+        needed in the game. (details depends on the implementation) */
+        this.itemDeck = dFactory.createItemDeck();
+        this.sectorDeck = dFactory.createSectorDeck();
+        this.hatchDeck = dFactory.createHatchDeck();
 
-        players = new ArrayList<>();
+        /* Populates a list of players with the characters in their
+        appropriate location. (details depends on the implementation) */
+        this.players = pFactory.createPlayers(pNumber, zone.getHumanSector(),
+                zone.getAlienSector());
+
         noises = new ArrayList<>();
+        turnNumber = 0;
 
     }
 
-    public static Game create(ZoneFactory zone, DecksFactory decks, int
-            playersNumber) {
+    /**
+     * Initializes a new Game, every part is populated with the provided
+     * factories, the turn number to zero and the list of noises empty.
+     *
+     * @param zFactory an object implementation of a {@link ZoneFactory} used
+     *                 to
+     *                 create the map of the Game
+     * @param dFactory an object implementation of a {@link DecksFactory} used
+     *                 to populate all the decks of the Game
+     * @param pFactory an object implementation of a {@link PlayersFactory}
+     *                 used
+     *                 to populate the list of players of the Game
+     * @param pNumber  the number of players in this Game
+     * @return a new initialized Game
+     */
+    public static Game initialize(ZoneFactory zFactory, DecksFactory
+            dFactory, PlayersFactory pFactory, int pNumber) {
 
-        Game game = new Game(zone, decks);
-        return null;
-        // TODO
+        return new Game(zFactory, dFactory, pFactory, pNumber);
 
     }
 
