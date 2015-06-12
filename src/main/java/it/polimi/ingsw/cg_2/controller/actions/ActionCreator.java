419066c1f72b2d5c816662bca0a2503299e425e8
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-12 15:39:12 +0200

    Create ActionFactoryVisitorInt interface

--- src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionCreator.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionCreator.java
@@ -2,6 +2,6 @@ package it.polimi.ingsw.cg_2.controller.actions;
 
 public interface ActionCreator {
 
-    Action createAction(ActionFactoryVisitor visitor);
+    Action createAction(ActionFactoryVisitorInt visitor);
 
 }
---
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionFactoryVisitorInt.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.cg_2.controller.actions;
+
+public interface ActionFactoryVisitorInt {
+
+    // Action visit(RequestActionMsg requestMsg);
+    // Action visit(RequestActionMsg requestMsg);
+    // Action visit(RequestActionMsg requestMsg);
+    // Action visit(RequestActionMsg requestMsg);
+    // Action visit(RequestActionMsg requestMsg);
+
+}
