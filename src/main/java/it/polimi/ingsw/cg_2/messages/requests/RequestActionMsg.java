138683ebe9b209d63db93318b8b854eb67baeed2
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-12 15:35:58 +0200

    Create RequestActionMsg class

---
+++ src/main/java/it/polimi/ingsw/cg_2/messages/requests/RequestActionMsg.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.cg_2.messages.requests;
+
+import it.polimi.ingsw.cg_2.controller.actions.ActionCreator;
+
+
+public abstract class RequestActionMsg implements ActionCreator {
+
+
+
+}
