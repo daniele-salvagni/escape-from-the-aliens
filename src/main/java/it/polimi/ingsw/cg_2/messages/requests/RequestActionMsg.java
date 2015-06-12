6f5010a143fb9f7a78ef0ad92777347f0fd03f3d
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-12 15:41:22 +0200

    Implement abstract class methods

--- src/main/java/it/polimi/ingsw/cg_2/messages/requests/RequestActionMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/requests/RequestActionMsg.java
@@ -5,6 +5,18 @@ import it.polimi.ingsw.cg_2.controller.actions.ActionCreator;
 
 public abstract class RequestActionMsg implements ActionCreator {
 
+    String token;
 
+    public RequestActionMsg(String token) {
+
+        this.token = token;
+
+    }
+
+    public String getToken() {
+
+        return token;
+
+    }
 
 }
