e586d57ae48ec41f4bef8374316bb6b8ae54f3d3
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-12 19:32:02 +0200

    Complete javadoc

--- src/main/java/it/polimi/ingsw/cg_2/controller/actions/Action.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/Action.java
@@ -13,18 +13,51 @@ import it.polimi.ingsw.cg_2.messages.responses.ResponseMsg;
  */
 public interface Action {
 
+    /**
+     * Check if the atomic action is valid according to the rules of the game.
+     * This checks only the single action, and not if the sequence is valid
+     * inside a turn (this is checked before following a "chain of
+     * responsibility" principle).
+     *
+     * @return true, if the atomic action is valid
+     */
     boolean isValid();
 
-    // Implementations does return TurnState, but we need to break
-    // dependencies between the client and the server somewhere. The Visitor
-    // pattern works very well in this case, but it creates a lot of
-    // dependencies. This can be done in different ways, but in every case it
-    // does include the necessity to cast an object.
+    /**
+     * Executes the action. After the execution it does return the following
+     * {@link it.polimi.ingsw.cg_2.controller.turn.TurnState} of the state
+     * machine that controls the sequence of actions. Null is returned if the
+     * state should not change (this keeps the state machine as simple as
+     * possible, and it removes the payload of carrying around the information
+     * about the current status).
+     * <p/>
+     * Implementations should always return a TurnStatus object, but we need to
+     * break the dependencies between the client and server somewhere. The
+     * visitor pattern works very well in this case, but it does introduce a
+     * lot
+     * of dependencies. There are other ways to break them but in every case it
+     * would involve the casting of an object and this is the place where it
+     * makes more sense.
+     *
+     * @return the next TurnState of the state machine controlling the sequence
+     * of actions
+     */
     Object execute();
 
+    /**
+     * Generates the response message to be sent to the client after the
+     * execution of the action.
+     *
+     * @return the private response message for the client
+     */
     ResponseMsg getRespondeMesage();
 
+    /**
+     * Generates a message to be broadcasted to all the clients of the game
+     * after the execution of the action.
+     *
+     * @return the public broadcast message for all the clients
+     */
     BroadcastMsg getBroadcastMessage();
-
 
 }
