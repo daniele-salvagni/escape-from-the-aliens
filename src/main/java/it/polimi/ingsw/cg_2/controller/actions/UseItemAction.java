a9ebb56f76dc7daa093e6f6bbc26c54d615923a1
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-13 02:27:13 +0200

    Add UseItemAction class

---
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/UseItemAction.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.cg_2.controller.actions;
+
+import it.polimi.ingsw.cg_2.messages.broadcast.BroadcastMsg;
+import it.polimi.ingsw.cg_2.messages.responses.ResponseMsg;
+import it.polimi.ingsw.cg_2.model.Game;
+
+/**
+ *
+ */
+public class UseItemAction implements Action {
+
+    @Override
+    public boolean isValid(Game game) {
+
+        return false;
+    }
+
+    @Override
+    public Object execute() {
+
+        return null;
+    }
+
+    @Override
+    public ResponseMsg getResponseMessage() {
+
+        return null;
+    }
+
+    @Override
+    public BroadcastMsg getBroadcastMessage() {
+
+        return null;
+    }
+}
