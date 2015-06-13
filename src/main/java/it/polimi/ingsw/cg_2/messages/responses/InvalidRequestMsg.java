08a89f169887b5afb042176d60430dd74a240344
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-13 11:32:50 +0200

    Add InvalidRequestMsg class

---
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/InvalidRequestMsg.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.cg_2.messages.responses;
+
+/**
+ *
+ */
+public class InvalidRequestMsg implements ResponseMsg {
+
+    public InvalidRequestMsg() {
+
+
+
+    }
+
+    public InvalidRequestMsg(String reason) {
+
+
+
+    }
+
+}
