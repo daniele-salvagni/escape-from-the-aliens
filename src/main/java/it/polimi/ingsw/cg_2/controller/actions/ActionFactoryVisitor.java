4d8cca14fe4ed840a1af059e07e15d84e2b73364
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-17 16:18:17 +0200

    Add Javadoc (to be implemented)

--- src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionFactoryVisitor.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionFactoryVisitor.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.cg_2.controller.actions;
 
+/**
+ * This factory uses the visitor pattern to create an appropriate {@link Action}
+ * from an
+ * {@link it.polimi.ingsw.cg_2.messages.requests.actions.ActionRequestMsg}
+ * without knowing the concrete type of the message object.
+ */
 public interface ActionFactoryVisitor {
 
     // TODO: It should map the token to the player (of model) when converting
