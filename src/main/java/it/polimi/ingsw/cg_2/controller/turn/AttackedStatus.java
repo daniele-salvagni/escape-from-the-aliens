81f2749af5ecf156150184523fed142c19d39d30
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 20:04:00 +0200

    Implement class basic structure

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/AttackedStatus.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/AttackedStatus.java
@@ -1,8 +1,20 @@
 package it.polimi.ingsw.cg_2.controller.turn;
 
-/**
- *
- */
-public class AttackedStatus {
+import it.polimi.ingsw.cg_2.controller.actions.Action;
+import it.polimi.ingsw.cg_2.model.Game;
 
+public enum AttackedStatus implements TurnState {
+    INSTANCE;
+
+    @Override
+    public boolean isActionValid(Action action, Game game) {
+
+        return false;
+    }
+
+    @Override
+    public TurnState executeAction(Action action, Game game) {
+
+        return null;
+    }
 }
