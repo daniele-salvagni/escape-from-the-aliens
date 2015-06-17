fbcd91119e75e31c82a8f009001ea612e0bcd273
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 20:15:46 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/TriedToEscapeState.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/TriedToEscapeState.java
@@ -3,6 +3,11 @@ package it.polimi.ingsw.cg_2.controller.turn;
 import it.polimi.ingsw.cg_2.controller.actions.Action;
 import it.polimi.ingsw.cg_2.model.Game;
 
+/**
+ * This is a state of the state machine that manages a game. It represents
+ * the state where a Player did try to escape while being inside an hatch sector
+ * without success.
+ */
 public enum TriedToEscapeState implements TurnState {
 
     INSTANCE;
