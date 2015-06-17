9d976fccf31a6be22562482bc5bc2a3a0097a426
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:59:19 +0200

    Improve javadoc

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/MadeNoiseState.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/MadeNoiseState.java
@@ -3,6 +3,11 @@ package it.polimi.ingsw.cg_2.controller.turn;
 import it.polimi.ingsw.cg_2.controller.actions.Action;
 import it.polimi.ingsw.cg_2.model.Game;
 
+/**
+ * This is a state of the state machine that manages a game. It represents
+ * the state where a Player has satisfied his Noise constraints after drawing a
+ * card.
+ */
 public enum MadeNoiseState implements TurnState {
 
     INSTANCE;
