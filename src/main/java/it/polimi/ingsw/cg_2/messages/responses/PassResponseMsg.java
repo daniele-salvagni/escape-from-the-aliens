89ef1ef3cf0592c3607d071968765795abac527a
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 22:02:31 +0200

    Implement base class structure

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/PassResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/PassResponseMsg.java
@@ -1,12 +1,23 @@
 package it.polimi.ingsw.cg_2.messages.responses;
 
+import it.polimi.ingsw.cg_2.controller.actions.Action;
+import it.polimi.ingsw.cg_2.controller.turn.TurnState;
+import it.polimi.ingsw.cg_2.model.Game;
 
+public enum PassResponseMsg implements TurnState {
 
+    INSTANCE;
 
+    @Override
+    public boolean isActionValid(Action action, Game game) {
 
+        return false;
+    }
 
+    @Override
+    public TurnState executeAction(Action action, Game game) {
 
-
-public class PassResponseMsg {
+        return null;
+    }
 
 }
