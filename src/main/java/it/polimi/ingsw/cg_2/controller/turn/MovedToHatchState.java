2851639dbc733a95ae38018cf009315d1d8ab27f
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 20:12:25 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/MovedToHatchState.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/MovedToHatchState.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.cg_2.controller.turn;
 import it.polimi.ingsw.cg_2.controller.actions.Action;
 import it.polimi.ingsw.cg_2.model.Game;
 
+/**
+ * This is a state of the state machine that manages a game. It represents
+ * the state where a Player has moved to an escape Hatch.
+ */
 public enum MovedToHatchState implements TurnState {
 
     INSTANCE;
