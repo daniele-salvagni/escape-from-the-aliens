971d66c336f1e12213d55ebc04c278a286e29b75
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:21:08 +0200

    Implement class methods and add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/InvalidRequestMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/InvalidRequestMsg.java
@@ -1,20 +1,40 @@
 package it.polimi.ingsw.cg_2.messages.responses;
 
 /**
- *
+ * This response message is sent to the client if his request was not valid. It
+ * could contain a reason (mainly for debugging purposes).
  */
 public class InvalidRequestMsg implements ResponseMsg {
 
+    private final String reason;
+
+    /**
+     * Creates a new InvalidRequestMsg without a reason.
+     */
     public InvalidRequestMsg() {
 
-
+        this(null);
 
     }
 
+    /**
+     * Creates a new InvalidRequestMsg containing a reason.
+     *
+     * @param reason the reason why the request was invalid
+     */
     public InvalidRequestMsg(String reason) {
 
-
+        this.reason = reason;
 
     }
 
+    /**
+     * Get the reason why the message is invalid.
+     *
+     * @return the reason why the request was invalid
+     */
+    public String getReason() {
+
+        return reason;
+    }
 }
