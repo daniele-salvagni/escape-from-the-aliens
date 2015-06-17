092e3fb56a77d4cd1e291afe4bd1aa8b39cd6a38
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:01:49 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/AttackResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/AttackResponseMsg.java
@@ -1,6 +1,11 @@
 package it.polimi.ingsw.cg_2.messages.responses;
 
-
+/**
+ * A response message for the AttackAction. Contains information about the
+ * position and the made kills.
+ */
 public class AttackResponseMsg {
+
+
 
 }
