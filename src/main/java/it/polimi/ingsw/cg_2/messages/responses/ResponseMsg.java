5158976abf1557cabe999cb830e8c1693cc08d01
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-17 15:57:21 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/ResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/ResponseMsg.java
@@ -1,9 +1,9 @@
 package it.polimi.ingsw.cg_2.messages.responses;
 
-
+/**
+ * This interface represents a Response message from the server to the client,
+ * subsequent to a client request.
+ */
 public interface ResponseMsg {
 
-
-
 }
-
