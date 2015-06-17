ce134488a0fe0671ba38c131b5af83af450e9ef4
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-17 16:22:57 +0200

    Change from Interface to Abstract, add information about execution

--- src/main/java/it/polimi/ingsw/cg_2/controller/actions/Action.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/Action.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.cg_2.controller.actions;
 
 import it.polimi.ingsw.cg_2.messages.ResultMsgPair;
-import it.polimi.ingsw.cg_2.model.Game;
 
 /**
  * This class represents an atomic Action (like a command pattern) to be
@@ -11,7 +10,21 @@ import it.polimi.ingsw.cg_2.model.Game;
  * to generate the private response for the client and the broadcast message to
  * inform all the players of the game.
  */
-public interface Action {
+public abstract class Action {
+
+    protected boolean executed;
+
+    // The result of the action
+    private ResultMsgPair resultMsgPair;
+
+    /**
+     * Constructor for a generic Action, sets the executed state to false.
+     */
+    protected Action() {
+
+        this.executed = false;
+
+    }
 
     /**
      * Check if the atomic action is valid according to the rules of the game.
@@ -21,7 +34,7 @@ public interface Action {
      *
      * @return true, if the atomic action is valid
      */
-    boolean isValid(Game game);
+    public abstract boolean isValid();
 
     /**
      * Executes the action. After the execution it does return the following
@@ -42,7 +55,18 @@ public interface Action {
      * @return the next TurnState of the state machine controlling the sequence
      * of actions
      */
-    Object execute(Game game);
+    public abstract Object execute();
+
+    /**
+     * Sets the result message pair of this action after execution.
+     *
+     * @param resultMsgPair the result message pair
+     */
+    protected void setMessagePair(ResultMsgPair resultMsgPair) {
+
+        this.resultMsgPair = resultMsgPair;
+
+    }
 
     /**
      * Returns both the private (response) and public (broadcast) result
@@ -50,6 +74,37 @@ public interface Action {
      *
      * @return the private and public result message pair
      */
-    ResultMsgPair getResult();
+    public ResultMsgPair getResult() {
+
+        if (!hasBeenExecuted()) {
+            // Could happen only by a programming error, we don't want to
+            // recover from this.
+            throw new AssertionError("Result can be obtained only after " +
+                    "execution.");
+        }
+
+        return resultMsgPair;
+
+    }
+
+    /**
+     * Set this action as already executed.
+     */
+    protected void setExecuted() {
+
+        executed = true;
+
+    }
+
+    /**
+     * Check if this action have already been executed.
+     *
+     * @return true, if the action have already been executed
+     */
+    protected boolean hasBeenExecuted() {
+
+        return executed;
+
+    }
 
 }
