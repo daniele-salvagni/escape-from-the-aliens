9fd4eee11ca4080fa13834034a588fd5a10e460c
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 20:01:41 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/DrawnDeceptionState.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/DrawnDeceptionState.java
@@ -3,6 +3,11 @@ package it.polimi.ingsw.cg_2.controller.turn;
 import it.polimi.ingsw.cg_2.controller.actions.Action;
 import it.polimi.ingsw.cg_2.model.Game;
 
+/**
+ * This is a state of the state machine that manages a game. It represents
+ * the state where a Player drawn a Deception card (so he must chose the
+ * position where to make the noise).
+ */
 public enum DrawnDeceptionState implements TurnState {
 
     INSTANCE;
@@ -18,4 +23,5 @@ public enum DrawnDeceptionState implements TurnState {
 
         return null;
     }
+
 }
