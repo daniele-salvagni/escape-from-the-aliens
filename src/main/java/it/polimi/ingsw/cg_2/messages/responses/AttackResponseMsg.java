702c64ada4c4783375868ac119bbfb0c9a7c13c1
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:08:43 +0200

    Add javadoc to methods

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/AttackResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/AttackResponseMsg.java
@@ -16,11 +16,21 @@ public class AttackResponseMsg implements ResponseMsg {
 
     }
 
+    /**
+     * Get the coordinate of the attack
+     *
+     * @return the coordinate of the attack
+     */
     public String getCoordinate() {
 
         return coordinate;
     }
 
+    /**
+     * Get the list of killed players (could be empty)
+     *
+     * @return the list of killed players
+     */
     public int[] getKills() {
 
         return kills;
