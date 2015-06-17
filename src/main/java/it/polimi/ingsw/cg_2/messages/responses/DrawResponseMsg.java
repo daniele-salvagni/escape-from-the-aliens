0df6fa7533d4263a2b0c79614506f581d2f419f7
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:05:34 +0200

    Add javadoc to class

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/DrawResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/DrawResponseMsg.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.cg_2.messages.responses;
 
-
+/**
+ * A response message for the DrawAction. Contains information about the
+ * type of card.
+ */
 public class DrawResponseMsg {
 
 
