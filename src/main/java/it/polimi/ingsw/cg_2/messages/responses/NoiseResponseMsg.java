48adf575977f46c40bd4484bc461569d97a40d2e
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:25:48 +0200

    Implement class methods

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/NoiseResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/NoiseResponseMsg.java
@@ -1,10 +1,18 @@
 package it.polimi.ingsw.cg_2.messages.responses;
 
-/**
- *
- */
+
 public class NoiseResponseMsg implements ResponseMsg {
 
+    private final String coordinate;
 
+    public NoiseResponseMsg(String coordinate) {
 
+        this.coordinate = coordinate;
+
+    }
+
+    public String getCoordinate() {
+
+        return coordinate;
+    }
 }
