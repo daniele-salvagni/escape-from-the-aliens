d884182644e4cc643cdcf37938ecb3a7e1cf35a2
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-17 19:34:34 +0200

    Create class MovedToDangerState

---
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/MovedToDangerState.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.cg_2.controller.turn;
+
+import it.polimi.ingsw.cg_2.controller.actions.Action;
+import it.polimi.ingsw.cg_2.model.Game;
+
+/**
+ *
+ */
+public enum MovedToDangerState implements TurnState {
+
+    INSTANCE;
+
+    @Override
+    public boolean isActionValid(Action action, Game game) {
+
+        return false;
+    }
+
+    @Override
+    public TurnState executeAction(Action action, Game game) {
+
+        return null;
+    }
+
+}
