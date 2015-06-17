32765effd9f48569099695bf82df8a434a880e32
Simone Triventi <simone.triventi@gmail.com>
2015-06-17 19:44:35 +0200

    Implement class basic structure

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/MadeNoise.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/MadeNoise.java
@@ -1,5 +1,22 @@
 package it.polimi.ingsw.cg_2.controller.turn;
 
-public class MadeNoise {
+import it.polimi.ingsw.cg_2.controller.actions.Action;
+import it.polimi.ingsw.cg_2.model.Game;
+
+public enum MadeNoise implements TurnState {
+
+    INSTANCE;
+
+    @Override
+    public boolean isActionValid(Action action, Game game) {
+
+        return false;
+    }
+
+    @Override
+    public TurnState executeAction(Action action, Game game) {
+
+        return null;
+    }
 
 }
