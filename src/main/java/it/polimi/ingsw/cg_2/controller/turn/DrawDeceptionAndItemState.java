b519644fd48fbc191e26f015bcd5174af62b6397
Simone Triventi <simone.triventi@gmail.com>
2015-06-19 00:11:36 +0200

    Implement singleton

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/DrawDeceptionAndItemState.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/DrawDeceptionAndItemState.java
@@ -8,6 +8,20 @@ import it.polimi.ingsw.cg_2.model.Game;
  */
 public class DrawDeceptionAndItemState extends TurnState {
 
+    private static final DrawDeceptionAndItemState INSTANCE = new
+            DrawDeceptionAndItemState();
+
+    /**
+     * Get the instance of the DrawDeceptionAndItemState singleton class.
+     *
+     * @return the instance of the singleton
+     */
+    public static DrawDeceptionAndItemState getInstance() {
+
+        return INSTANCE;
+
+    }
+
     @Override
     public boolean isActionValid(Action action, Game game) {
 
