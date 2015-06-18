261bbc315d0ed1945cede967b8dfb8247565e913
Simone Triventi <simone.triventi@gmail.com>
2015-06-19 00:10:51 +0200

    Extend TurnState

--- src/main/java/it/polimi/ingsw/cg_2/controller/turn/DrawDeceptionAndItemState.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/turn/DrawDeceptionAndItemState.java
@@ -1,8 +1,17 @@
 package it.polimi.ingsw.cg_2.controller.turn;
 
+import it.polimi.ingsw.cg_2.controller.actions.Action;
+import it.polimi.ingsw.cg_2.model.Game;
+
 /**
  *
  */
-public class DrawDeceptionAndItemState {
+public class DrawDeceptionAndItemState extends TurnState {
+
+    @Override
+    public boolean isActionValid(Action action, Game game) {
+
+        return false;
+    }
 
 }
