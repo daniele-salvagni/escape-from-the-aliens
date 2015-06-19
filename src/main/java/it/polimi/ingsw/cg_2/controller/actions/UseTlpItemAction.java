a11f6396cc4dfb2c6773521b0de38d42c60d3893
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-19 16:05:10 +0200

    Create UseTlpItemAction class

---
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/UseTlpItemAction.java
@@ -0,0 +1,51 @@
+package it.polimi.ingsw.cg_2.controller.actions;
+
+import it.polimi.ingsw.cg_2.model.Game;
+import it.polimi.ingsw.cg_2.model.deck.Deck;
+import it.polimi.ingsw.cg_2.model.deck.ItemCard;
+import it.polimi.ingsw.cg_2.model.player.Player;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class UseTlpItemAction extends UseItemAction {
+
+    private static final Logger LOG = Logger.getLogger(UseTlpItemAction.class
+            .getName());
+
+    public UseTlpItemAction(Game game, Player player) {
+
+        super(game, player, ItemCard.ItemCardType.TELEPORT);
+
+    }
+
+    @Override
+    public Object execute() {
+
+        /* These situations could happen only by programming errors, we don't
+         want to recover from that. */
+        if (super.hasBeenExecuted()) {
+            throw new AssertionError("An action should be executed only once");
+        } else if (!isValid()) {
+            throw new AssertionError("An action should be executed only if " +
+                    "valid");
+        }
+
+        // Set this action as executed, so it cannot be executed another time
+        super.setExecuted();
+
+        // Remove the item from the player and put it back in the deck
+        Deck<ItemCard> itemDeck = getGame().getItemDeck();
+        ItemCard item = getPlayer().removeItem(getItemType());
+        itemDeck.discardCard(item);
+
+        getPlayer().getCharacter().setPosition(getGame().getZone()
+                .getHumanSector());
+
+        return null;
+
+    }
+
+}
