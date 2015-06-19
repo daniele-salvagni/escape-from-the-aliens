5a9d23532050550c74eef8015e9a9ca9d1b51aa7
Simone Triventi <simone.triventi@gmail.com>
2015-06-19 19:50:50 +0200

    Implement useAtkItemResponseMsg class methods

--- src/main/java/it/polimi/ingsw/cg_2/messages/responses/UseAtkItemResponseMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/UseAtkItemResponseMsg.java
@@ -8,5 +8,35 @@ import java.util.Map;
  */
 public class UseAtkItemResponseMsg implements ResponseMsg {
 
+    private final String coordinate;
+    private final Map<Integer, String> kills;
+    private final List<Integer> survivors;
+
+    public UseAtkItemResponseMsg(String coordinate, Map<Integer, String>
+            kills, List<Integer> survivors) {
+
+        this.coordinate = coordinate;
+        this.kills = kills;
+        this.survivors = survivors;
+
+    }
+
+    public String getCoordinate() {
+
+        return coordinate;
+
+    }
+
+    public Map<Integer, String> getKills() {
+
+        return kills;
+
+    }
+
+    public List<Integer> getSurvivors() {
+
+        return survivors;
+
+    }
 
 }
