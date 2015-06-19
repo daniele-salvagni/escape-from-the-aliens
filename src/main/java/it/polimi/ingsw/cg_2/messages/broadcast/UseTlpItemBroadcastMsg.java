5b9c161d5637cb583f4cb802bb2f59343613ec68
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-19 19:25:15 +0200

    Implement UseTlpItemBroadcastMsg class

---
+++ src/main/java/it/polimi/ingsw/cg_2/messages/broadcast/UseTlpItemBroadcastMsg.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.cg_2.messages.broadcast;
+
+/**
+ * Created by dan on 19/06/15.
+ */
+public class UseTlpItemBroadcastMsg implements BroadcastMsg {
+
+    int player;
+    String coordinate;
+
+    public UseTlpItemBroadcastMsg(int player, String coordinate) {
+
+        this.player = player;
+        this.coordinate = coordinate;
+
+    }
+
+    public int getPlayer() {
+
+        return player;
+
+    }
+
+    public String getCoordinate() {
+
+        return coordinate;
+
+    }
+
+}
