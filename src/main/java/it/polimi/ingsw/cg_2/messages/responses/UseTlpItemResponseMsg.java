e6da370e998e1c54f988c963bbd91451a34f82ee
Simone Triventi <simone.triventi@gmail.com>
2015-06-19 19:52:21 +0200

    Implement UseTlpItemResponseMsg class methods

---
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/UseTlpItemResponseMsg.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.cg_2.messages.responses;
+
+/**
+ *
+ */
+public class UseTlpItemResponseMsg {
+
+    String coordinate;
+
+    public UseTlpItemResponseMsg(String coordinate) {
+
+        this.coordinate = coordinate;
+
+    }
+
+    public String getCoordinate() {
+
+        return coordinate;
+
+    }
+
+}
