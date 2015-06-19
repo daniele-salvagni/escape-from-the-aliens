14d1e16ff52b5cf94f17676159341fa19612f490
Simone Triventi <simone.triventi@gmail.com>
2015-06-19 11:31:17 +0200

    Implement class methods

--- src/main/java/it/polimi/ingsw/cg_2/controller/actions/EscapeAction.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/EscapeAction.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw.cg_2.controller.actions;
 
+import it.polimi.ingsw.cg_2.controller.turn.TriedToEscapeState;
+import it.polimi.ingsw.cg_2.controller.turn.TurnStartedState;
 import it.polimi.ingsw.cg_2.model.Game;
+import it.polimi.ingsw.cg_2.model.deck.Deck;
+import it.polimi.ingsw.cg_2.model.deck.HatchCard;
+import it.polimi.ingsw.cg_2.model.deck.ItemCard;
 import it.polimi.ingsw.cg_2.model.player.CharacterRace;
 import it.polimi.ingsw.cg_2.model.player.Player;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -50,7 +56,91 @@ public class EscapeAction extends Action {
     @Override
     public Object execute() {
 
-        return null;
+        /* These situations could happen only by programming errors, we don't
+         want to recover from that. */
+        if (super.hasBeenExecuted()) {
+            throw new AssertionError("An action should be executed only once");
+        } else if (!isValid()) {
+            throw new AssertionError("An action should be executed only if " +
+                    "valid");
+        }
+
+        // Set this action as executed, so it cannot be executed another time
+        super.setExecuted();
+
+        Deck<HatchCard> hatchDeck = game.getHatchDeck();
+
+        // Draw a Sector Card, store it...
+        HatchCard hatchCard = hatchDeck.drawCard();
+        // ... and put it back in the deck
+        hatchDeck.discardCard(hatchCard);
+
+        if (hatchCard.getType() == HatchCard.HatchCardType.GREEN) {
+
+            // Player escaped and turn goes to the next player
+
+            Player nextPlayer = escape(player);
+            game.setCurrentPlayer(nextPlayer);
+
+            // Create a response result for this action,
+            setMessagePair(ResponseFactory.escapeResponse(game, player,
+                    hatchCard, nextPlayer));
+
+            return TurnStartedState.getInstance();
+
+        } else { // RED
+
+            // Create a response result for this action,
+            setMessagePair(ResponseFactory.escapeResponse(game, player,
+                    hatchCard, null));
+
+            return TriedToEscapeState.getInstance();
+
+        }
+
+
+    }
+
+    /**
+     * Set a player as escaped, put back his items in the deck, deactivate the
+     * activated items and find the following player.
+     *
+     * @param player the player that escapes
+     * @return the next player
+     */
+    private Player escape(Player player) {
+
+        Deck<ItemCard> itemDeck = game.getItemDeck();
+        List<ItemCard> playerItems = player.getHeldItems();
+
+        player.getCharacter().setEscaped(true);
+
+        for (ItemCard item : playerItems) {
+
+            itemDeck.discardCard(item);
+            player.removeItem(item);
+
+        }
+
+        player.clearActiveItems();
+
+        int playerNumber = game.getPlayerNumber(player);
+        List<Player> players = game.getPlayers();
+
+        Player nextPlayer = player;
+
+        // Find the next player
+        for (int i = 1; i < players.size(); i++) {
+            if (!player.isSuspended() && player.isConnected() && !player
+                    .getCharacter().isEscaped() && player.getCharacter()
+                    .isAlive()) {
+                players.get((i + playerNumber) % players.size());
+            }
+        }
+
+        // It could return the same player, but in this case the game will
+        // end immediately after this.
+        return nextPlayer;
 
     }
 
