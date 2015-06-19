dc21236a25e8ffbdc8457fb62d10cff8364a5b2c
Simone Triventi <simone.triventi@gmail.com>
2015-06-19 19:50:36 +0200

    Create useAtkItemResponseMsg class

---
+++ src/main/java/it/polimi/ingsw/cg_2/messages/responses/UseAtkItemResponseMsg.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.cg_2.messages.responses;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+public class UseAtkItemResponseMsg implements ResponseMsg {
+
+
+}
