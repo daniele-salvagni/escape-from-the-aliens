96fa342015283df77bc82dae954300dc25dadda2
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-19 19:09:48 +0200

    Implement NoiseBroadcastMsg class

--- src/main/java/it/polimi/ingsw/cg_2/messages/broadcast/NoiseBroadcastMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/broadcast/NoiseBroadcastMsg.java
@@ -5,4 +5,43 @@ package it.polimi.ingsw.cg_2.messages.broadcast;
  */
 public class NoiseBroadcastMsg {
 
+    private final int player;
+    private final String cardType;
+    private final String coordinate;
+    private final boolean item;
+
+    public NoiseBroadcastMsg(int player, String cardType, String coordinate,
+            boolean item) {
+
+        this.player = player;
+        this.cardType = cardType;
+        this.coordinate = coordinate;
+        this.item = item;
+
+    }
+
+    public int getPlayer() {
+
+        return player;
+
+    }
+
+    public String getCardType() {
+
+        return cardType;
+
+    }
+
+    public String getCoordinate() {
+
+        return coordinate;
+
+    }
+
+    public boolean isItem() {
+
+        return item;
+
+    }
+
 }
