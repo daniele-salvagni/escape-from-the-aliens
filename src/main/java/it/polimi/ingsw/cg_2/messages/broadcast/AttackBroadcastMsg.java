b1556b975dd9d54a29a4dbe45eb9293f6cee18c5
daniele-salvagni <daniele.salvagni@gmail.com>
2015-06-19 19:06:52 +0200

    Implement AttackBroadcastMsg class

--- src/main/java/it/polimi/ingsw/cg_2/messages/broadcast/AttackBroadcastMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/broadcast/AttackBroadcastMsg.java
@@ -1,8 +1,50 @@
 package it.polimi.ingsw.cg_2.messages.broadcast;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  */
-public class AttackBroadcastMsg {
+public class AttackBroadcastMsg implements BroadcastMsg {
+
+    private final int player;
+    private final String coordinate;
+    private final Map<Integer, String> kills;
+    private final List<Integer> survivors;
+
+    public AttackBroadcastMsg(int player, String coordinate, Map<Integer,
+            String> kills, List<Integer> survivors) {
+
+        this.player = player;
+        this.coordinate = coordinate;
+        this.kills = kills;
+        this.survivors = survivors;
+
+    }
+
+    public int getPlayer() {
+
+        return player;
+
+    }
+
+    public String getCoordinate() {
+
+        return coordinate;
+
+    }
+
+    public Map<Integer, String> getKills() {
+
+        return kills;
+
+    }
+
+    public List<Integer> getSurvivors() {
+
+        return survivors;
+
+    }
 
 }
