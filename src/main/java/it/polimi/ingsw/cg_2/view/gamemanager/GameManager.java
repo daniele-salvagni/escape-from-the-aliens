d065c6accfc101858fe2c7bca0b872555aa24152
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-21 22:22:03 +0200

    Create ServerInitializer class

--- src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/GameManager.java
+++ src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/GameManager.java
@@ -5,4 +5,11 @@ package it.polimi.ingsw.cg_2.view.gamemanager;
  */
 public class GameManager {
 
+    public static void main(String[] args) {
+
+        ServerInitializer serverInitializer = new ServerInitializer();
+
+
+    }
+
 }
