7b8fad9a9846f8fbb9722480477f8e390f420dc5
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-21 17:31:10 +0200

    Implement class methods

--- src/main/java/it/polimi/ingsw/cg_2/messages/requests/actions/DrawRequestMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/requests/actions/DrawRequestMsg.java
@@ -9,4 +9,17 @@ import it.polimi.ingsw.cg_2.controller.actions.ActionFactoryVisitor;
 public class DrawRequestMsg extends ActionRequestMsg {
 
 
+    public DrawRequestMsg(String token) {
+
+        super(token);
+
+    }
+
+    @Override
+    public Action createAction(ActionFactoryVisitor visitor) {
+
+        return visitor.visit(this);
+
+    }
+
 }
