3fc1dc457a1ce23485305dfd70eeb711eb4f9de6
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-21 17:41:17 +0200

    Implement ActionFactoryVisitor interface

--- src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionFactoryVisitor.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/actions/ActionFactoryVisitor.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.cg_2.controller.actions;
 
-import it.polimi.ingsw.cg_2.messages.requests.actions.AttackRequestMsg;
-import it.polimi.ingsw.cg_2.messages.requests.actions.MoveRequestMsg;
+import it.polimi.ingsw.cg_2.messages.requests.actions.*;
 
 /**
- * This factory uses the visitor pattern to create an appropriate {@link Action}
+ * This factory uses the visitor pattern to create an appropriate {@link
+ * Action}
  * from an
  * {@link it.polimi.ingsw.cg_2.messages.requests.actions.ActionRequestMsg}
  * without knowing the concrete type of the message object.
@@ -12,25 +12,28 @@ import it.polimi.ingsw.cg_2.messages.requests.actions.MoveRequestMsg;
 public interface ActionFactoryVisitor {
 
     // TODO: It should map the token to the player (of model) when converting
-    // from msg to action
-
-    // Action visit(ActionRequestMsg requestMsg);
-    // Action visit(ActionRequestMsg requestMsg);
-    // Action visit(ActionRequestMsg requestMsg);
-    // Action visit(ActionRequestMsg requestMsg);
-    // Action visit(ActionRequestMsg requestMsg);
-
+    // from msg to action (implementation)
 
     Action visit(AttackRequestMsg requestMsg);
+
     Action visit(DrawRequestMsg requestMsg);
+
     Action visit(EscapeRequestMsg requestMsg);
+
     Action visit(MoveRequestMsg requestMsg);
+
     Action visit(NoiseRequestMsg requestMsg);
+
     Action visit(PassRequestMsg requestMsg);
+
     Action visit(UseAdrRequestMsg requestMsg);
-    Action visit(USeAtkRequestMsg requestMsg);
+
+    Action visit(UseAtkRequestMsg requestMsg);
+
     Action visit(UseSdtRequestMsg requestMsg);
+
     Action visit(UseSptRequestMsg requestMsg);
+
     Action visit(UseTlpRequestMsg requestMsg);
 
 }
