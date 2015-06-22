88bb6286cf74b6055908bc670908ef30e3f7914a
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-22 18:07:18 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/cg_2/view/gameplayer/PlayerConnectionFactory.java
+++ src/main/java/it/polimi/ingsw/cg_2/view/gameplayer/PlayerConnectionFactory.java
@@ -3,18 +3,30 @@ package it.polimi.ingsw.cg_2.view.gameplayer;
 import it.polimi.ingsw.cg_2.messages.Token;
 import it.polimi.ingsw.cg_2.view.commons.BrokerInterface;
 import it.polimi.ingsw.cg_2.view.commons.RequestHandler;
+import it.polimi.ingsw.cg_2.view.commons.SubscriberInterface;
 
 /**
- *
+ * Abstract class for a PlayerConnectionFactory.
  */
 public abstract class PlayerConnectionFactory {
+
+    protected final static int RMI_PORT = 7777;
 
     private final ViewUpdater viewUpdater;
     private final Subscriber subscriber;
 
     private Token token;
 
+    /**
+     * Abstract class constructor for the PlayerConnectionFactory class.
+     *
+     * @param viewUpdater a view updater to display updates from the server
+     */
     public PlayerConnectionFactory(ViewUpdater viewUpdater) {
+
+        if (viewUpdater == null) {
+            throw new IllegalArgumentException("ViewUpdater must not be null.");
+        }
 
         this.viewUpdater = viewUpdater;
         subscriber = new Subscriber(viewUpdater);
@@ -23,16 +35,29 @@ public abstract class PlayerConnectionFactory {
 
     }
 
+    /**
+     * Setup a new connection to the game manager as a new client.
+     */
     public void setupConnection() {
 
         token = getRequestHandler().connect();
 
-        getBrokerInterface().subscribe(getSubscriber(), token);
+        getBrokerInterface().subscribe(getSubscriberInterface(), token);
 
     }
 
+    /**
+     * Get the remote request handler.
+     *
+     * @return the remote request handler
+     */
     public abstract RequestHandler getRequestHandler();
 
+    /**
+     * Get the remote broker interface.
+     *
+     * @return the remote broker interface
+     */
     public abstract BrokerInterface getBrokerInterface();
 
     public void setToken(Token token) {
@@ -41,21 +66,47 @@ public abstract class PlayerConnectionFactory {
 
     }
 
+    /**
+     * Get the token that identifies the client.
+     *
+     * @return the token that identifies the client
+     */
     public Token getToken() {
 
         return token;
 
     }
 
+    /**
+     * Get the view updater.
+     *
+     * @return the view updater
+     */
     public ViewUpdater getViewUpdater() {
 
         return viewUpdater;
 
     }
 
+    /**
+     * Get the Subscriber.
+     *
+     * @return the subscriber
+     */
     public Subscriber getSubscriber() {
 
         return subscriber;
+
+    }
+
+    /**
+     * Get the subscriber interface
+     *
+     * @return the subscriber interface
+     */
+    public SubscriberInterface getSubscriberInterface() {
+
+        return (SubscriberInterface)subscriber;
 
     }
 
