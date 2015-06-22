1860544545005cdaa44406411177e375325d8d6f
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-23 01:10:24 +0200

    Create SocketFactory class

---
+++ src/main/java/it/polimi/ingsw/cg_2/view/gameplayer/SocketFactory.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.cg_2.view.gameplayer;
+
+import it.polimi.ingsw.cg_2.view.commons.BrokerInterface;
+import it.polimi.ingsw.cg_2.view.commons.RequestHandler;
+
+/**
+ *
+ */
+public class SocketFactory extends PlayerConnectionFactory {
+
+    @Override
+    public RequestHandler getRequestHandler() {
+
+        return null;
+    }
+
+    @Override
+    public BrokerInterface getBrokerInterface() {
+
+        return null;
+    }
+}
