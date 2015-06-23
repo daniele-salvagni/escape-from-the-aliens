0bffbe1448b3fc536713b26276d5d8d6556486df
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-23 16:19:20 +0200

    Add throws and unsubscribeClientFromTopic to the interface

--- src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/PublisherInterface.java
+++ src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/PublisherInterface.java
@@ -12,18 +12,20 @@ public interface PublisherInterface {
 
     void publish(BroadcastMsg message, String topic);
 
-    void addTopic(String topic);
+    void addTopic(String topic) throws IllegalArgumentException;
 
-    void addTopic(String topic, Set<Token> clients);
+    void addTopic(String topic, Set<Token> clients) throws IllegalArgumentException;
 
     void subscribeClientToTopic(String topic, Token client);
 
     void subscribeClientsToTopic(String topic, Set<Token> clients);
 
+    void unsubscribeClientFromTopic(String topic, Token client);
+
     void removeClient(Token client);
 
     void removeClients(Set<Token> clients);
 
-    void removeTopic(String topic);
+    void removeTopic(String topic) throws IllegalArgumentException;
 
 }
