736c988646acff550286a83d532dc626e1d3d425
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-23 16:18:33 +0200

    Implement abstract class Sockethandler

--- src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/SocketHandler.java
+++ src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/SocketHandler.java
@@ -1,8 +1,34 @@
 package it.polimi.ingsw.cg_2.view.gamemanager;
 
+import java.net.Socket;
+
 /**
- *
+ * This abstract class contain a basic structure to handle a socket connection. Implements
+ * Runnable for multithreading.
  */
-public class SocketHandler {
+abstract class SocketHandler implements Runnable {
+
+    private final Socket socket;
+
+    /**
+     * Abstract constructor for a new SocketHandler.
+     *
+     * @param socket the Socket connection
+     */
+    SocketHandler(Socket socket) {
+
+        this.socket = socket;
+    }
+
+    /**
+     * Get the Socket connection.
+     *
+     * @return the Socket connection
+     */
+    protected Socket getSocket() {
+
+        return socket;
+
+    }
 
 }
