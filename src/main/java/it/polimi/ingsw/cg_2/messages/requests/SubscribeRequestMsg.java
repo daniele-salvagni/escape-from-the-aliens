47b0fd63474c49f76b0220dee370083c21b13ab4
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-23 21:53:10 +0200

    Add Topic to SubscribeRequestMsg

--- src/main/java/it/polimi/ingsw/cg_2/messages/requests/SubscribeRequestMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/requests/SubscribeRequestMsg.java
@@ -7,9 +7,18 @@ import it.polimi.ingsw.cg_2.messages.Token;
  */
 public class SubscribeRequestMsg extends RequestMsg {
 
-    public SubscribeRequestMsg(Token token) {
+    private final String topic;
+
+    public SubscribeRequestMsg(Token token, String topic) {
 
         super(token);
+        this.topic = topic;
+
+    }
+
+    public String getTopic() {
+
+        return topic;
 
     }
 
