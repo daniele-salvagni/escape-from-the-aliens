54f136984a015422d8124952277c98a5c9122aba
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-23 16:43:54 +0200

    Create ClientHandler class

---
+++ src/main/java/it/polimi/ingsw/cg_2/view/gamemanager/ClientHandler.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.cg_2.view.gamemanager;
+
+/**
+ * Handles the connection with the client for request-response messages communication.
+ */
+public class ClientHandler extends SocketHandler {
+
+    @Override
+    public void run() {
+
+    }
+}
