64e05c11e36df76a9a53d8c9d4273d5ca25ad2d5
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-24 15:27:00 +0200

    Create GamesController class to implement RequestHandler

---
+++ src/main/java/it/polimi/ingsw/cg_2/controller/GamesController.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.cg_2.controller;
+
+import it.polimi.ingsw.cg_2.messages.Token;
+import it.polimi.ingsw.cg_2.messages.requests.RequestMsg;
+import it.polimi.ingsw.cg_2.messages.responses.ResponseMsg;
+import it.polimi.ingsw.cg_2.view.commons.RequestHandler;
+
+import java.rmi.RemoteException;
+
+/**
+ *
+ */
+public class GamesController implements RequestHandler {
+
+    @Override
+    public Token connect() throws RemoteException {
+
+        return null;
+    }
+
+    @Override
+    public ResponseMsg processRequest(RequestMsg request) throws RemoteException {
+
+        return null;
+    }
+
+}
