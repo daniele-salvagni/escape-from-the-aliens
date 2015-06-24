83612fe7634ee57ba78b24d9db02e2b3a0c615fb
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-25 00:36:29 +0200

    Add UUID to Token

--- src/main/java/it/polimi/ingsw/cg_2/messages/Token.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/Token.java
@@ -1,8 +1,24 @@
 package it.polimi.ingsw.cg_2.messages;
 
+import java.io.Serializable;
+
 /**
  *
  */
-public class Token {
+public class Token implements Serializable {
+
+    private final String uuid;
+
+    public Token(String uuid) {
+
+        this.uuid = uuid;
+
+    }
+
+    public String getUuid() {
+
+        return uuid;
+
+    }
 
 }
