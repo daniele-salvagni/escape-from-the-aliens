0ff29649f98e12be828d0c5bc6ccbeba7e83778e
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-24 14:01:55 +0200

    Remove topic

--- src/main/java/it/polimi/ingsw/cg_2/messages/requests/SubscribeRequestMsg.java
+++ src/main/java/it/polimi/ingsw/cg_2/messages/requests/SubscribeRequestMsg.java
@@ -7,18 +7,9 @@ import it.polimi.ingsw.cg_2.messages.Token;
  */
 public class SubscribeRequestMsg extends RequestMsg {
 
-    private final String topic;
-
-    public SubscribeRequestMsg(Token token, String topic) {
+    public SubscribeRequestMsg(Token token) {
 
         super(token);
-        this.topic = topic;
-
-    }
-
-    public String getTopic() {
-
-        return topic;
 
     }
 
