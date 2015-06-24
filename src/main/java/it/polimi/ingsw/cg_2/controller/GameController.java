754ea3bb8603bafa88525e23a1377f061ff4eeb1
Daniele Salvagni <daniele.salvagni@gmail.com>
2015-06-25 00:56:00 +0200

    Implement some class methods

--- src/main/java/it/polimi/ingsw/cg_2/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/cg_2/controller/GameController.java
@@ -4,11 +4,24 @@ import it.polimi.ingsw.cg_2.controller.actions.Action;
 import it.polimi.ingsw.cg_2.controller.actions.ActionFactoryVisitor;
 import it.polimi.ingsw.cg_2.controller.turn.TurnMachine;
 import it.polimi.ingsw.cg_2.messages.ResultMsgPair;
+import it.polimi.ingsw.cg_2.messages.Token;
+import it.polimi.ingsw.cg_2.messages.requests.RequestMsg;
 import it.polimi.ingsw.cg_2.messages.requests.actions.ActionRequestMsg;
 import it.polimi.ingsw.cg_2.messages.responses.InvalidRequestMsg;
+import it.polimi.ingsw.cg_2.messages.responses.ResponseMsg;
 import it.polimi.ingsw.cg_2.model.Game;
+import it.polimi.ingsw.cg_2.model.deck.DecksFactory;
+import it.polimi.ingsw.cg_2.model.deck.StandardDecksFactory;
+import it.polimi.ingsw.cg_2.model.map.ZoneFactory;
+import it.polimi.ingsw.cg_2.model.map.ZoneLoader;
+import it.polimi.ingsw.cg_2.model.map.ZoneName;
+import it.polimi.ingsw.cg_2.model.player.PlayersFactory;
+import it.polimi.ingsw.cg_2.model.player.StandardPlayersFactory;
 import it.polimi.ingsw.cg_2.utils.exception.InvalidMsgException;
+import it.polimi.ingsw.cg_2.view.gamemanager.PublisherInterface;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,10 +33,63 @@ public class GameController {
     private static final Logger LOG = Logger.getLogger(GameController.class
             .getName());
 
-    Game game;
-    TurnMachine turnMachine;
+    private static int numberOfGames = 1;
+    private final int gameID;
 
-    ActionFactoryVisitor actionFactory;
+    private Game game;
+    private TurnMachine turnMachine;
+    private ActionFactoryVisitor actionFactory;
+
+    private final List<Token> players;
+
+    private DecksFactory dFactory;
+    private PlayersFactory pFactory;
+    private ZoneFactory zFactory;
+
+    private final PublisherInterface publisherInterface;
+
+    public GameController(List<Token> players, PublisherInterface publisherInterface) {
+
+        // Make a copy, minimize mutability
+        this.players = new ArrayList<>(players);
+        this.publisherInterface = publisherInterface;
+
+        dFactory = new StandardDecksFactory();
+        pFactory = new StandardPlayersFactory();
+
+        // This is just the default map, can be changed
+        zFactory = ZoneFactory.newLoader(ZoneName.GALILEI);
+
+        gameID = numberOfGames;
+        numberOfGames++;
+
+    }
+
+    public void initGame() {
+
+        game = Game.initialize(zFactory, dFactory, pFactory, players.size());
+        turnMachine = new TurnMachine(game);
+
+    }
+
+    public int getGameID() {
+
+        return gameID;
+
+    }
+
+    public String getTopic() {
+
+        return "GAME" + Integer.toString(gameID);
+
+    }
+
+    private boolean gameFinished() {
+
+        // TODO
+        return false;
+
+    }
 
     /**
      * Handle a request from the client, a Message is passed to a Factory that
@@ -34,7 +100,7 @@ public class GameController {
      * @param request the request message from the client
      * @return the result of the execution of the message (response & broadcast)
      */
-    public ResultMsgPair handleRequest(ActionRequestMsg request) {
+    public ResultMsgPair executeAction(ActionRequestMsg request) {
 
         Action action;
 
@@ -51,11 +117,46 @@ public class GameController {
 
             LOG.log(Level.WARNING, "Handled an invalid request.", e);
             // Notify the client that the message is not valid
-            return new ResultMsgPair(new InvalidRequestMsg("INVALID"), null);
+            return new ResultMsgPair(new InvalidRequestMsg("Invalid request content."),
+                    null);
 
         }
 
 
     }
+
+    private boolean isPlayerTurn(Token token) {
+
+        return players.indexOf(token) == game.getCurrentPlayerNumber();
+
+    }
+
+    public ResponseMsg handleRequest(RequestMsg request) {
+
+        if (game == null) {
+            return new InvalidRequestMsg("Please wait for the game to start.");
+        }
+
+        if (!isPlayerTurn(request.getToken())) {
+            return new InvalidRequestMsg("Please wait your turn to play.");
+        }
+
+        if (request instanceof ActionRequestMsg) {
+
+            ResultMsgPair result = executeAction((ActionRequestMsg) request);
+
+            if (result.broadcast != null) {
+                publisherInterface.publish(result.broadcast, getTopic());
+            }
+
+            return result.response;
+
+        }
+
+        // TODO: Other Requests (not actions, if any)
+        return null;
+
+    }
+
 
 }
